3bc2a100dc6f3c2f61823f9093f0fdf0e1808db9
Shun Git <kwokshungit@gmail.com>
2017-10-19 10:59:45 +0800

    move access_token out of initwelcome.sh

--- initwelcome.sh
+++ initwelcome.sh
@@ -3,13 +3,20 @@
 # initwelcome.sh: sets welcome screen
 # Execute once only
 
+if [ ! -f access_token ]; then
+    echo "Error! Please put your Facebook access_token in ./access_token"
+    exit 1
+fi
+
+ACCESS_TOKEN="$( cat ./access_token )"
+
 # Greeting Text
 curl -X POST -H "Content-Type: application/json" -d '{
   "setting_type":"greeting",
   "greeting":{
     "text":"Hello {{user_first_name}}, welcome to NewsBot."
   }
-}' "https://graph.facebook.com/v2.6/me/thread_settings?access_token=EAAEaTYHPhpEBAPZA8olwdCxhsZAx5ZCJgc88lnXTsEOIP5LEqJtsrhJTix9H9CXVNZCWSuN3GtzSvaTMosfIQmE4VZAjHFkKiAGjrYxyY3AoR14ZBMY2Gz6dLKV1KYBqMfVMGRZCzBqEUOflhkuBUGShg8AKdCiW4HyadTHcv0yswZDZD"
+}' "https://graph.facebook.com/v2.6/me/thread_settings?access_token=${ACCESS_TOKEN}"
 
 # Get Started button
 curl -X POST -H "Content-Type: application/json" -d '{
@@ -20,4 +27,4 @@ curl -X POST -H "Content-Type: application/json" -d '{
       "payload":"Get Started"
     }
   ]
-}' "https://graph.facebook.com/v2.6/me/thread_settings?access_token=EAAEaTYHPhpEBAPZA8olwdCxhsZAx5ZCJgc88lnXTsEOIP5LEqJtsrhJTix9H9CXVNZCWSuN3GtzSvaTMosfIQmE4VZAjHFkKiAGjrYxyY3AoR14ZBMY2Gz6dLKV1KYBqMfVMGRZCzBqEUOflhkuBUGShg8AKdCiW4HyadTHcv0yswZDZD"
+}' "https://graph.facebook.com/v2.6/me/thread_settings?access_token=${ACCESS_TOKEN}"
